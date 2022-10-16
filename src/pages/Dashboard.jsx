abd181a90f2772e5a54af2e7d881d815164f3f71
mariel <90232128+honeyjoycest@users.noreply.github.com>
2022-10-16 16:59:20 +0800

    react

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
     return ( 
         <div className="dashboard text-secondary font-link">
-             <h2 className="text-center m-4 "> <img src="image/mdc logo.png" alt="" width="70px" height="70px" position="center"/> Mater Dei College Information System </h2> 
+             <h2 className="text-center m-4 "> <img src="image/mdc logo.png" alt="" width="70px" height="70px" position="center"/> Mater Dei College Student Information System </h2> 
              <h1> <img src="image/mdc.png" alt="" width="900px" height="500px" position="center"/></h1> 
         </div>
      );
